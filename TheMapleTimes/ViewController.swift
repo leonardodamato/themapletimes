10679a2b60041a183dc3ff8fc196ffad4f93a497
Leonardo D'Amato <leonardodamato@outlook.com>
2021-05-28 18:48:28 +1000

    Initial Commit

---
+++ TheMapleTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMapleTimes
+//
+//  Created by Leonardo D'Amato on 28/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
